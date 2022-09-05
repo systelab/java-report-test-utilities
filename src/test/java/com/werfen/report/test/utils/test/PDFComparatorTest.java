f61c7471f78727b27c9d89a10fb77d75d728dd4a
Guillermo Martinez <gmartinez3@werfen.com>
2022-09-05 16:59:32 +0200

    Adds tests for compareStreams method.

--- src/test/java/com/werfen/report/test/utils/test/PDFComparatorTest.java
+++ src/test/java/com/werfen/report/test/utils/test/PDFComparatorTest.java
@@ -1,10 +1,14 @@
 package com.werfen.report.test.utils.test;
 
 import com.werfen.report.test.utils.assertions.ComparisonResultAssertions;
+import com.werfen.report.test.utils.pdf.PDFComparator;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static com.werfen.report.test.utils.pdf.PDFComparator.compareFiles;
 
@@ -15,14 +19,27 @@ public class PDFComparatorTest {
     @Test
     public void compareFilesEqual() throws IOException {
         File file = new File(RESOURCE_PATH + "file1.pdf");
-        ComparisonResultAssertions.assertEquals(compareFiles(file, file));
+        ComparisonResultAssertions.assertEquals(PDFComparator.compareFiles(file, file));
     }
 
+    @Test
+    public void compareStreamsEqual() throws IOException {
+        InputStream expectedStream = Files.newInputStream(Paths.get(RESOURCE_PATH + "file1.pdf"));
+        InputStream actualStream = Files.newInputStream(Paths.get(RESOURCE_PATH + "file1.clone.pdf"));
+        ComparisonResultAssertions.assertEquals(PDFComparator.compareStreams(expectedStream, actualStream));
+    }
 
     @Test
     public void compareFilesNotEqual() throws IOException {
         File expectedFile = new File(RESOURCE_PATH + "file1.pdf");
         File actualFile = new File(RESOURCE_PATH + "file2.pdf");
         ComparisonResultAssertions.assertNotEquals(compareFiles(expectedFile, actualFile));
+    }
+
+    @Test
+    public void compareStreamsNotEqual() throws IOException {
+        InputStream expectedStream = Files.newInputStream(Paths.get(RESOURCE_PATH + "file1.pdf"));
+        InputStream actualStream = Files.newInputStream(Paths.get(RESOURCE_PATH + "file2.pdf"));
+        ComparisonResultAssertions.assertNotEquals(PDFComparator.compareStreams(expectedStream, actualStream));
     }
 }
